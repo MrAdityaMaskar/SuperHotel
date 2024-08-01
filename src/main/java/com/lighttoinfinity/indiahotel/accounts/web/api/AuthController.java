84fe1201236dde0c313e3713e0349ebbc73ca912
Aditya Maskar <aditya01092001@gmail.com>
2024-08-01 16:30:55 +0530

    Added Frontend

--- src/main/java/com/lighttoinfinity/indiahotel/accounts/web/api/AuthController.java
+++ src/main/java/com/lighttoinfinity/indiahotel/accounts/web/api/AuthController.java
@@ -21,7 +21,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/auth")
+@RequestMapping("api/v1/auth")
 public class AuthController {
 
     private final AuthService authService;
